33e7fca556ebd6159cfb3a97de8f43684bb36f14
0x4C1D <0x4C1d@protonmail.com>
2023-01-04 20:24:27 +0000

    Alchemy week 2 - giftlist

--- client/index.js
+++ client/index.js
@@ -1,14 +1,22 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
 const MerkleTree = require('../utils/MerkleTree');
-
+const readline = require('readline-sync');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
 
+  const name = readline.question("Enter your name: ");
+  const tree = new MerkleTree(niceList)
+  const root = tree.getRoot();
+  const index = niceList.findIndex(n => n === name);
+
+  const proof = tree.getProof(index);
+  
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof: proof,
+    name: name,
   });
 
   console.log({ gift });
